b97e585bfb2ac29ae40e62ea2b480edee71b9d2d
laurentiu.miu@metro.digital <>
2021-05-26 13:18:52 +0300

    second commit

--- src/main/java/home/rent/rig4jump/repository/EquipmentRepository.java
+++ src/main/java/home/rent/rig4jump/repository/EquipmentRepository.java
@@ -1,9 +1,11 @@
-package home.rent.rig4jump;
+package home.rent.rig4jump.repository;
 
 import home.rent.rig4jump.model.Equipment;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@Tag(name = "1. Equipment Endpoints")
 @RepositoryRestResource
 public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
 }
